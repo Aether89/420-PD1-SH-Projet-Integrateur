0e55be58b0cc2a87960502c705791de85463ea20
Aether89 <dev@aether89.com>
2023-09-07 08:42:08 -0400

    2309
    /lescitronsdetoto-frontend/src/components/
    - suppression de HelloWorld car remplacer par VehicleList
    - ajout de VehiclesList et VehicleListItem pour afficher la liste des véhicules.
    
    /lescitronsdetoto-frontend/src/services/common.js
    - ajout de fonctioner pour formatter le prix à afficher
    
    /lescitronsdetoto-frontend/src/views/Home.vue
    - changement de HelloWorld pour utiliser VehicleList.

---
+++ lescitronsdetoto-frontend/src/components/VehicleListItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-card color="amber" class="pa-2">
+    <v-img aspect-ratio="1/1" :src="carImg"></v-img>
+    <div class="card-content">
+      <p class="text-left two-line">{{ vehicleName }}</p>
+      <p class="text-right">{{ formatedPrice }}</p>
+    </div>
+    <router-link :to="carURL" aria-label="Voir plus" class="text-h6">Voir plus</router-link>
+  </v-card>
+</template>
+
+<script>
+import {priceFormatting} from '@/services/common';
+
+export default {
+  props: ['img', 'make', 'model', 'year', 'price', 'id'],
+  data() {
+    return {};
+  },
+  computed: {
+
+    carURL() {
+      return "/vehicle/" + this.id;
+    },
+    vehicleName() {
+      return this.make + " " + this.model + " " + this.year;
+    },
+    carImg() {
+      return (this.img !== null) ? this.img : "./src/assets/lemon.png";
+    },
+    formatedPrice() {
+      return priceFormatting(this.price);
+    }
+  }
+}
+</script>
+  
+<style scoped>
+.card-content {
+  height: 3em;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.two-line {
+  line-height: 1.5em;
+  overflow: hidden;
+  white-space: normal;
+}
+</style>
---
+++ lescitronsdetoto-frontend/src/components/VehiclesList.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container>
+      <v-row class="flex d-flex">
+        <v-col align-self-center cols="12" xs="4" sm="3" lg="2" v-for="vehicle in store" class="flex d-flex flex-column">
+          <VehicleListItem :img="vehicle.img" :id="vehicle.id" :make="vehicle.make" :model="vehicle.model"
+            :price="vehicle.price" :year="vehicle.year">
+          </VehicleListItem>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+
+<script>
+import VehicleListItem from '@/components/VehicleListItem.vue';
+import store from '@/store';
+import { useVehiclesStore } from '@/store/vehicles';
+export default {
+    components: {
+        VehicleListItem: VehicleListItem
+    },
+    data() {
+        return {
+            store: useVehiclesStore().vehicles,
+        };
+    },
+    mounted() {
+        console.log("Vehicles List")
+        console.log(JSON.stringify(this.vehicles,null,"  "));
+    }
+}
+</script>
