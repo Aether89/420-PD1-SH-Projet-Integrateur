40bf5c445c7f01d526bb61612b7071ae1e468d5c
Aether89 <dev@aether89.com>
2023-09-12 22:04:15 -0400

    correctif component detailled-vehicle pour utiliser :id="this.vin" au lieux de :id="this.id"

--- lescitronsdetoto-frontend/src/components/VehicleListItem.vue
+++ lescitronsdetoto-frontend/src/components/VehicleListItem.vue
@@ -24,7 +24,7 @@
             Voir plus
           </v-btn>
         </template>
-        <detailled-vehicle class="mb-8" :id="this.id" :isDialog="true" @close-dialog="this.dialog = !this.dialog"></detailled-vehicle>
+        <detailled-vehicle class="mb-8" :id="this.vin" :isDialog="true" @close-dialog="this.dialog = !this.dialog"></detailled-vehicle>
       </v-dialog>
       
   </v-card>
