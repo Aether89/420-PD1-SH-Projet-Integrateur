9068f6b467c932a796dbb87326e5522f511d8023
Aether89 <dev@aether89.com>
2023-10-24 14:13:34 -0400

    Update ListeEmployes.vue

--- lescitronsdetoto-frontend/src/views/ListeEmployes.vue
+++ lescitronsdetoto-frontend/src/views/ListeEmployes.vue
@@ -1,15 +1,16 @@
-<template v-if="this.session.isAdmin">
-    <v-container class="d-flex block" justify="center">
-
+<template >
+    <v-container v-if="this.session.user && this.session.user.isAdmin"  class="d-flex block" justify="center">
         <listeEmploye />
-
         <EmployeVue />
-
+    </v-container>
+    <v-container v-else class="d-flex block" justify="center">
+        vous n'avez pas les droits pour acceder à cette page
     </v-container>
 </template>
 <script>
 import listeEmploye from '@/components/employeComponent/listeEmploye.vue';
 import EmployeVue from '@/components/employeComponent/Employe.vue';
+import session from '@/session';
 
 export default {
     components: {
@@ -17,10 +18,9 @@ export default {
         EmployeVue: EmployeVue,
     },
 
-
     data() {
         return {
-
+            session: session,
         };
     },
 };
